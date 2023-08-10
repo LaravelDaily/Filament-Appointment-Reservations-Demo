5ef81e209f8dae4a47379abf59827f816d7b1661
David Lun <mysticcode@gmail.com>
2023-08-10 16:26:47 +0300

    Set Up AppLayout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+        <meta name="application-name" content="{{ config('app.name') }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        @filamentStyles
+        @vite('resources/css/app.css')
+    </head>
+
+    <body class="antialiased bg-gray-100">
+        <div class="max-w-7xl mx-auto">
+            {{ $slot }}
+        </div>
+
+
+        @filamentScripts
+        @vite('resources/js/app.js')
+    </body>
+</html>
